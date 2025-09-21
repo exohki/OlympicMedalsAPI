d0ade8e1385471bc27b777b80d6d59eec7a7f6f0
emileemontijo <emontijo@my.wctc.edu>
2025-09-21 16:26:23 -0500

    changes

--- Program.cs
+++ Program.cs
@@ -3,27 +3,32 @@ using OlympicMedalsAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// SQLite database configuration
 builder.Services.AddDbContext<OlympicContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("OlympicDb") 
         ?? "Data Source=olympicmedals.db"));
 
+// Add controllers and Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Ensure database is created and migrations applied
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<OlympicContext>();
     db.Database.Migrate();
 }
 
-if (app.Environment.IsDevelopment() || true) 
+// Enable Swagger for all environments and make it accessible at root
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "OlympicMedalsAPI v1");
+    c.RoutePrefix = string.Empty; // Swagger will open at https://<your-app>.azurewebsites.net/
+});
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
